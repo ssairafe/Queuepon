45fcd52a75dd1715b84b4e79e1b29ac1f9e930a7
Saria Sairafe <sariaphd@gmail.com>
2019-07-09 17:51:32 -0700

    the actual final for lfz standards

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(initializeApp);
 
 function average(num1, num2) {
  var avg = (num1 / num2) * 100;
- return avg;
+  return avg.toFixed(2);
 }
 
 var firstCard = null;
@@ -12,20 +12,61 @@ var attempts = 0;
 var accuracy = 0;
 var maxMatches = 9;
 var matches = null;
+
 function resetStats() {
   firstCard= null;
   secondCard= null;
   matches= null;
   attempts= null;
+  $(".container").children('.cardHolder').remove();
+  shuffle(cardArray);
+  addCards();
+  $(".back").css("display", "block");
   $("#attempts").text("0");
   $("#accuracy").text("0" + " %");
-  $(".back").css("display", "block");
   $("#modulBack").css("display", "none");
   $("#modul").css("display", "none");
 }
 
-function initializeApp() {
+var cardArray = ['target', 'amazon', 'starbucks', 'playstation', 'dominos', 'bmw', 'apple', 'lafit', 'android', 'target', 'amazon', 'starbucks', 'playstation', 'dominos', 'bmw', 'apple', 'lafit', 'android'];
+
+function shuffle (array) {
+  var currentIndex = array.length;
+  var tempValue , randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    tempValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = tempValue;
+  }
+  return array;
+}
+
+function addCards () {
+  for (var i = 0; i < cardArray.length; i++){
+  var companyName = cardArray[i];
+  var backCard = $('<div>', {
+    class: 'back ',
+    company: companyName
+  }).text("?");
+  var frontCard = $('<div>', {
+    class: 'front ' + companyName
+  });
+  var card = $('<div>', {
+    class: 'cardHolder'
+  });
+    card.append(frontCard, backCard);
+  $('.container').append(card);
+  }
   $(".back").on("click", handleClick);
+
+}
+function initializeApp() {
+  shuffle(cardArray);
+  addCards();
 }
 
 function handleClick(event) {
